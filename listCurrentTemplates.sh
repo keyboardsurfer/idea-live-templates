8acc926052f4694f39c0e082f700ad31a8760a1f
Benjamin Weiss <keyboardsurfer@google.com>
2015-02-05 14:30:05 +0000

    Improve maintainability of readme doc creation

--- listCurrentTemplates.sh
+++ listCurrentTemplates.sh
@@ -2,6 +2,11 @@
 for file in `ls -1 Android*.xml`; do
   echo
   echo "### $file"
-  cat $file | grep "template name" | sed s/"^.*<template name=\""/"- \*\*\`\`"/ | sed s/"toReformat.*$"// | sed s/"\".*description="/"\`\`\*\*: "/ | sort
+  cat $file | \
+    grep "template name" | \
+    sed s/"^.*<template name=\""/"- \*\*\`"/ | \
+    sed s/" toReformat.*$"// | \
+    sed s/"\".*description="/"\`\*\*: "/ | \
+    sort
 done
 
